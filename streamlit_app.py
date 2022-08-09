a5e5c73607ca8de11fa12641a764d7fc3875ce17
Nicholas Castañeda Espinel <42701839+nichocases@users.noreply.github.com>
2022-08-08 23:54:06 -0500

    Update streamlit_app.py
    new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Second line from Nicholas')
+streamlit.text('Hard Boiled Free Range Egg')
